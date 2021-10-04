2ac952f6d7322fbbdaf5ced9de82ce49922a0b2c
Kuki Amarilla <sc.amarilla@gmail.com>
2021-10-04 20:58:28 -0300

    Aumentado la distancia entre volantes

--- resources/views/volantes.blade.php
+++ resources/views/volantes.blade.php
@@ -10,6 +10,7 @@
                 <img src="https://static.wixstatic.com/media/0c505f_c8004f1cd4de466485f409e974dbf15a~mv2.jpeg/v1/fill/w_687,h_687,al_c,q_85/WhatsApp%20Image%202021-10-01%20at%2016_23_08.webp" alt="">
             </div>
         </div>
+        <br>
         <div class="volante">
             <div class="image-container">
                 <img src="https://static.wixstatic.com/media/0c505f_7e092593f5bc49d7a3f46f2ed07e8cb1~mv2.jpg/v1/fill/w_936,h_936,al_c,q_85,usm_0.66_1.00_0.01/volante.webp" alt="">
